f668cf52215ea1f63243451b211e1a785f434858
Rodion Clepa <rclepa>
2025-07-30 10:44:40 +0300

    feat(hero): add infinite carousel for stats

---
+++ src/script.js
@@ -0,0 +1,17 @@
+// const scrollContainer = document.querySelector("#stats-list");
+// let scrollSpeed = 2; // Adjust for desired speed
+// console.log(scrollContainer);
+//
+// function autoScroll() {
+//   console.log("autoScroll");
+//   if (
+//     scrollContainer.scrollLeft + scrollContainer.clientWidth >=
+//     scrollContainer.scrollWidth
+//   ) {
+//     scrollContainer.scrollLeft = 0;
+//   } else {
+//     scrollContainer.scrollLeft += scrollSpeed;
+//   }
+// }
+//
+// setInterval(autoScroll, 20);
