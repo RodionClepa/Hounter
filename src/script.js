ecce3872986c11f47827b0ac3da4daa0e62d41ba
Rodion Clepa <rclepa>
2025-08-01 11:37:13 +0300

    feat(recommendation): add draggable slider

--- src/script.js
+++ src/script.js
@@ -1,6 +1,7 @@
 import { RecommendationView } from "./recommendations/recommendation.view.js";
 import { RecommendationModel } from "./recommendations/recommendaiton.model.js";
 import { RecommendationController } from "./recommendations/recommendation.controller.js";
+import { DraggableSlider } from "./components/draggable-slider.js";
 
 const cloneCarouselContent = function (nodeRef, parentNode) {
   var copy = document.querySelector(nodeRef).cloneNode(true);
@@ -14,9 +15,16 @@ const cloneCarouselContent = function (nodeRef, parentNode) {
 
 const init = function () {
   cloneCarouselContent(".hero__stat-list", ".stat-carousel");
+
+  const draggableSlider = new DraggableSlider(
+    ".slider__container",
+    ".slider__item",
+  );
+  const recommendationModel = new RecommendationModel();
+  const recommendationView = new RecommendationView();
+  const recommendationController = new RecommendationController(
+    recommendationModel,
+    recommendationView,
+  );
 };
 init();
-
-const model = new RecommendationModel();
-const view = new RecommendationView();
-const controller = new RecommendationController(model, view);
