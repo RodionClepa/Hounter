01083f7c9b593c3c5fac25743a7e0e91f4ac51de
Rodion Clepa <rclepa>
2025-08-01 09:23:39 +0300

    fix(hero): move starting animation in js logic

--- src/script.js
+++ src/script.js
@@ -1,2 +1,7 @@
-var copy = document.querySelector(".hero__stat-list").cloneNode(true);
-document.querySelector(".stat-carousel").appendChild(copy);
+const copy = document.querySelector(".hero__stat-list").cloneNode(true);
+const parent = document.querySelector(".stat-carousel");
+parent.appendChild(copy);
+const tracks = parent.querySelectorAll(".carousel-track");
+if (tracks) {
+  tracks.forEach((track) => track.classList.add("animate"));
+}
