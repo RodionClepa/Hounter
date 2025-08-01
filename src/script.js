55592aa9d7f399da87b030fb81289dec320e4983
Rodion Clepa <rclepa>
2025-08-01 09:14:55 +0300

    feat(recommendation): add hidden slider item to prevent flickering

--- src/script.js
+++ src/script.js
@@ -1,6 +1,7 @@
 import { RecommendationView } from "./recommendations/recommendation.view.js";
 import { RecommendationModel } from "./recommendations/recommendaiton.model.js";
 import { RecommendationController } from "./recommendations/recommendation.controller.js";
+
 const cloneCarouselContent = function (nodeRef, parentNode) {
   var copy = document.querySelector(nodeRef).cloneNode(true);
   const parent = document.querySelector(parentNode);
