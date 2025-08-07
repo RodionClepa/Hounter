64fdc71f9ef3c1895d75197a894361166c99c718
Rodion Clepa <rclepa>
2025-08-07 11:05:14 +0300

    feat(reviews): add view render for slide show

--- src/reviews/reviews.model.js
+++ src/reviews/reviews.model.js
@@ -1,10 +1,25 @@
-export class Review {
+import { Model } from "../Model.js";
+import { apiService } from "../services/api.service.js";
+import { DATA_URL } from "../config.js";
+
+export class ReviewModel extends Model {
   eventTypes = {
     dataChange: "changeData",
   };
 
   constructor() {
+    super();
+    this.eventBus.updateEventTypes(this.eventTypes);
     this._data = [];
-    this._listeners = [];
+  }
+
+  async fetchData() {
+    try {
+      const data = await apiService.get(`${DATA_URL}reviews.json`);
+      this._data = data;
+      this.eventBus.notify(this.eventTypes.dataChange, this._data);
+    } catch (error) {
+      console.error("Failed to load recommendations:", error);
+    }
   }
 }
